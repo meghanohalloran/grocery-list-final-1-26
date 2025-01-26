3e3cc711fa5e244c8b5a735e11f62f3bf9f3682a
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-26 15:05:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const groceryList = [
+  { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
+  { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
+  { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
+  { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+  { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
+  { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
+  { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
+];
+
+fruitsArray = ["apple", "banana","orange", "broccoli", "milk", "cheddar", "sourdough"
+]
+
+for(let i = 0; i < fruitsArray.length; i++){
+  console.log(fruitsArray[i]);
+
+}
+
+var array = ["apple", "banana","orange", "broccoli", "milk", "cheddar", "sourdough"];
+
+var toUpperCase = array.map(function(name) { return name.toUpperCase(); });
+console.log(toUpperCase);
+
+
+// logNames => Takes in an array of items and console.logs the name of each item in the given array. Hint: use .forEach
+// 
+// getUpperCaseNames => Takes in an array of items and returns an array of item names in all uppercase. Hint: use .map
+// 
+// getItemById => Takes in an array of items and an id. Returns the item in the array with the given id. Hint: use .find
+// 
+// getItemPriceByName => Takes in an array of items and a name string. Returns the price of the item that matches the name that was passed in. Hint: use a loop
+// 
+// getItemsByCategory => Takes in an array of items and a category string. Returns an array of items that belong to the given `category`. Hint: use .filter 
+// 
+// countItems => Takes in an array of items and returns the total quantity of all items. Hint: use .reduce
+// 
+// calculateTotalPrice => Takes in an array of items and returns the cost of all given items. Hint: use .reduce
+// 
+// !!!!!!!!!!!Don't forget to console log as you go and test out each function!!!!!!!!!
+// Push to GitHub
+
+// // function logNames(name){
+// //   console.log(name);
+// // }
+// // names.forEach(logNames);
+
